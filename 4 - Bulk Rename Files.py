743df703fe2b776687725aae736fbd41de522d30
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2022-03-28 03:46:06 +0530

    Code Update in Project 4

--- 4 - Bulk Rename Files.py
+++ 4 - Bulk Rename Files.py
@@ -9,8 +9,15 @@ name_prefix = input("Enter the name prefix: ")
 all_files = os.listdir(folder_path)
 print("Old Filenames:", all_files)
 
+ignore_files = [".git"]
+
 count = 1
 for file in all_files:
-    file_extension = os.path.splitext(file)[1]
-    new_name = name_prefix + str(count) + file_extension
-    os.rename(folder_path + "/" + file, folder_path + "/" + new_name)
+    if file not in ignore_files:
+        file_extension = os.path.splitext(file)[1]
+        new_name = name_prefix + str(count) + file_extension
+        os.rename(folder_path + "/" + file, folder_path + "/" + new_name)
+        count += 1
+
+all_files = os.listdir(folder_path)
+print("New Filenames:", all_files)
